456212bad1054c9534d8222d8237c7969988d3fb
student <student@K04-08.lan.itstep.kiev.ua>
2018-05-17 21:14:28 +0300

    C

---
+++ Student/src/org/itstep/AppRunner.java
@@ -0,0 +1,12 @@
+package org.itstep;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+	StudentData studetData = StudentData("Misa", "Lysenko");
+	System.out.println("Your first name" + studentData.getFirstName());
+	System.out.println("Your second name"+studentData.getSecondName());
+	}
+
+
+}
